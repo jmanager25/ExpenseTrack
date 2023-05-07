a94e8c8f556f0fe30b322664bc6853963985054d
jmanager25 <>
2023-05-07 18:17:29 +0000

    Add the import urls in the main epensetrack file

--- expensetrack/urls.py
+++ expensetrack/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from expensetracker.views import home
+from django.urls import path, include
+from expensetracker import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home')
+    path('', views.home, name='home'),
+    path('', include('expensetracker.urls'),
+         name='expensetracker.urls'),
 ]
