de91f07d6914c559edf9423ccc1af4c619393e6e
jmanager25 <>
2023-05-07 21:39:20 +0000

    Update the home view to allow users to see transactions added

--- expensetracker/views.py
+++ expensetracker/views.py
@@ -5,14 +5,16 @@ from .models import Transaction, Category
 
 def home(request):
     """
-    View that returs the home page
+    View that returs the home page and allows user to see the 
+    transaction list.
     """
-    return render(request, "index.html")
+    transactions = Transaction.objects.filter(user=request.user)
+    return render(request, "index.html", {"transactions": transactions})
 
 
 class TransactionCreateView(CreateView):
     """
-    View that allows to add transactions
+    View that allows users to add transactions.
     """
     model = Transaction
     template_name = "add_transaction.html"
@@ -21,7 +23,7 @@ class TransactionCreateView(CreateView):
     def get_context_data(self, **kwargs):
         """
         Allows the transaction choises to be dinamically 
-        rendered in the template
+        rendered in the template.
         """
         context = super().get_context_data(**kwargs)
         context['transaction_type_choices'
