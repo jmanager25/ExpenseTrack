76758c19cba9e13644bdb47a6942674cf426cb45
jmanager25 <>
2023-05-09 21:54:20 +0000

    Add view to register users

--- user/views.py
+++ user/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 
+def register(request):
+    """
+    Allows users to make the registrantion on the webpage
+    """
+    if request.method == "GET":
+        return render(request, 'register.html')
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+    user = User.objects.create_user(
+        username=username, email=email, password=password)
+    user.save()
