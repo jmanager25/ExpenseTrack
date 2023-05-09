66900e3f3e0d575425dc090de8b9f7ebca1aa62f
jmanager25 <>
2023-05-09 23:09:40 +0000

    Add register.html template

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', views.login, name='login')
-}
+]
