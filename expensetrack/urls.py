e4be2156389b58a2eb17801afc53fd5699e56878
jmanager25 <>
2023-05-18 13:02:21 +0000

    Add a new app for savings

--- expensetrack/urls.py
+++ expensetrack/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('expensetracker.urls'),
          name='expensetracker.urls'),
+    path('savings', include('savings.urls'),
+         name='savings.urls'),
     path('account/', include('user.urls')),
 ]
