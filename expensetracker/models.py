31ffc84150a7af3e880d8a53eec551472c247bfa
jmanager25 <>
2023-05-18 16:41:17 +0000

    Add saving goal field to the transaction model

--- expensetracker/models.py
+++ expensetracker/models.py
@@ -10,6 +10,19 @@ class Category(models.Model):
         return self.name
 
 
+class Savings(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    target_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    target_date = models.DateField(default=now)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
 class Transaction(models.Model):
     INCOME = 'Income'
     EXPENSE = 'Expense'
@@ -27,21 +40,10 @@ class Transaction(models.Model):
     date = models.DateField(default=now)
     transaction_type = models.CharField(
         max_length=20, choices=TRANSACTION_TYPE_CHOICES)
+    saving_goal = models.ForeignKey(Savings, on_delete=models.CASCADE,
+                                    null=True, blank=True)
 
     objects = models.Manager()
 
     def __str__(self):
         return self.transaction_type
-
-
-class Savings(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
-    target_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    target_date = models.DateField(default=now)
-
-    objects = models.Manager()
-
-    def __str__(self):
-        return self.name
