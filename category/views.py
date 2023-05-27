ffc537914c5bc54fcf21a01f6e9b25ad51a6686f
jmanager25 <jmanager25@hotmail.com>
2023-05-27 16:11:59 +0000

    Add category view and template

--- category/views.py
+++ category/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from expensetracker.models import Category
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required(login_url='login')
+def category(request):
+    """
+    View that returs the categories page.
+    """
+    return render(request, 'category.html')
