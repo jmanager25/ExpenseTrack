0ee9fcab4a6c3345f5d565ca9631bc491c58ae54
jmanager25 <jmanager25@hotmail.com>
2023-05-27 16:27:15 +0000

    Add functionality to create categories

--- category/urls.py
+++ category/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.category, name='category'),
+    path('add/', views.CategoryCreateView.as_view(),
+         name='add_category'),
 ]
--- category/views.py
+++ category/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from expensetracker.models import Category
 from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 
 @login_required(login_url='login')
@@ -9,3 +13,19 @@ def category(request):
     View that returs the categories page.
     """
     return render(request, 'category.html')
+
+
+class CategoryCreateView(LoginRequiredMixin, CreateView):
+    """
+    View that allows users to create category.
+    """
+    model = Category
+    template_name = "add_category.html"
+    fields = ['name']
+    success_url = reverse_lazy('category')
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        messages.success(self.request, 'Category created successfully!')
+        return super().form_valid(form)
